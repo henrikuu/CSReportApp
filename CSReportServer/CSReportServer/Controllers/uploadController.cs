d68a090d24ed018a41b40519d969eaf66481a4f5
henrikuu <henri.kuusirati@gmail.com>
2013-11-04 17:52:18 +0200

    Added an ASP.NET server with an upload controller

---
+++ CSReportServer/CSReportServer/Controllers/uploadController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace CSReportServer.Controllers
+{
+    public class uploadController : Controller
+    {
+        //
+        // GET: /upload/
+
+        private bool success = false;
+
+        public ActionResult Index()
+        {
+
+            try
+            {
+                foreach (string fileInfo in Request.Files)
+                {
+                    string savePath = AppDomain.CurrentDomain.BaseDirectory + "\\uploadFolder";
+                    string filename = Request.Files[fileInfo].FileName;
+
+                    if (!Directory.Exists(savePath))
+                        Directory.CreateDirectory(savePath);
+
+                    Request.Files[fileInfo].SaveAs(savePath + "\\" + filename);
+
+                    success = true;
+                }
+            }
+            catch (HttpException e)
+            {
+                Console.WriteLine("HttpException: " + e.Message);
+            }
+
+            if (success)
+                this.ViewData.Add("uploadSuccess", true);
+            else
+                this.ViewData.Add("uploadSuccess", false);
+
+            return View();
+        }
+
+    }
+}
---
+++ CSReportServer/CSReportServer/runServer.bat
@@ -0,0 +1,2 @@
+@ECHO Korjaa polut etta toimii. Eka polku osoittaa severisoftan exeen. Toinen polku serveriprojektin kansioon jossa on Global.asax (tama kansio). Valitse myos vapaa portti. Testausta varten Fiddlerin composerissa laheta POST-viesti osoitteeseen "http://localhost:PORTTI/upload"
+"C:\Program Files (x86)\Common Files\Microsoft Shared\DevServer\11.0\WebDev.WebServer40.exe" /path:"C:\Misc\Dropbox\git\CSReportApp\CSReportServer\CSReportServer" /port:39506
