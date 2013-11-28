7007c669b2d838d2924262d8bcd2e4cb7d3c3735
henrikuu <henri.kuusirati@gmail.com>
2013-11-28 23:05:36 +0200

    Added support for saving pictures to isolated storage.
    Added support for hardware camera buttons. Added autofocus. Cleaned up
    camera layout. Added picture confirmation view.

--- CSReportApp/CSReportApp/PhotoCapturePage.xaml.cs
+++ CSReportApp/CSReportApp/PhotoCapturePage.xaml.cs
@@ -9,12 +9,16 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using Microsoft.Devices;
 using System.Windows.Media;
+using System.IO.IsolatedStorage;
+using System.IO;
 
 namespace CSReportApp
 {
     public partial class PhotoCapturePage : PhoneApplicationPage
     {
         PhotoCamera camera;
+        private bool focusAndShoot = false;
+        private int counter = 0;
 
         public PhotoCapturePage()
         {
@@ -29,9 +33,24 @@ namespace CSReportApp
                 photoViewfinderVideoBrush.SetSource(camera);
 
                 photoViewfinderVideoBrush.RelativeTransform = new CompositeTransform() { CenterX = 0.5, CenterY = 0.5, Rotation = 90 };
+
+                camera.CaptureCompleted += new EventHandler<CameraOperationCompletedEventArgs>(captureCompletedHandler);
+                camera.CaptureImageAvailable += new EventHandler<Microsoft.Devices.ContentReadyEventArgs>(captureImageAvailableHandler);
+                camera.CaptureThumbnailAvailable += new EventHandler<ContentReadyEventArgs>(captureThumbnailAvailableHandler);
+                camera.AutoFocusCompleted += new EventHandler<CameraOperationCompletedEventArgs>(autoFocusCompletedHandler);
+
+                CameraButtons.ShutterKeyHalfPressed += onButtonHalfPressHandler;
+                CameraButtons.ShutterKeyPressed += onButtonFullPressHandler;
+                CameraButtons.ShutterKeyReleased += onButtonReleaseHandler;
+
             }
             else
             {
+                Deployment.Current.Dispatcher.BeginInvoke(delegate()
+                 {
+                     MessageBox.Show("Camera not supported in this phone");
+                 });
+
                 NavigationService.GoBack();
             }
         }
@@ -41,20 +60,160 @@ namespace CSReportApp
             if (camera != null)
             {
                 camera.Dispose();
+                camera.CaptureImageAvailable -= captureImageAvailableHandler;
+                camera.CaptureCompleted -= captureCompletedHandler;
+                camera.CaptureThumbnailAvailable -= captureThumbnailAvailableHandler;
+                camera.AutoFocusCompleted -= autoFocusCompletedHandler;
+
+                CameraButtons.ShutterKeyHalfPressed -= onButtonHalfPressHandler;
+                CameraButtons.ShutterKeyPressed -= onButtonFullPressHandler;
+                CameraButtons.ShutterKeyReleased -= onButtonReleaseHandler;
             }
         }
 
         private void takePhotoButton_Click(object sender, RoutedEventArgs e)
         {
-            //if (camera != null)
-            //{
-            //    camera.CaptureImage();
-            //}
+            try
+            {
+                if (camera != null)
+                {
+                    focusAndShoot = true;
+                    camera.Focus();
+                }
+            }
+            catch (Exception) { }
+        }
+
+        void autoFocusCompletedHandler(object sender, CameraOperationCompletedEventArgs e)
+        {
+            if (focusAndShoot)
+            {
+                focusAndShoot = false;
+
+                try
+                {
+                    camera.CaptureImage();
+                }
+                catch (Exception) { }
+            }
         }
 
         private void cancelButton_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.GoBack();
+            Dispatcher.BeginInvoke(() => NavigationService.GoBack());
+        }
+
+        void captureCompletedHandler(object sender, CameraOperationCompletedEventArgs e)
+        {
+            counter++;
+        }
+        
+        public void captureThumbnailAvailableHandler(object sender, ContentReadyEventArgs e)
+        {
+            string fileName = counter + "_th.jpg";
+
+            try
+            {
+                using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    using (IsolatedStorageFileStream stream = isf.OpenFile(fileName, FileMode.Create, FileAccess.Write))
+                    {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead = -1;
+
+                        while ((bytesRead = e.ImageStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            stream.Write(buffer, 0, bytesRead);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                e.ImageStream.Close();
+            }
+        }
+        
+        void captureImageAvailableHandler(object sender, Microsoft.Devices.ContentReadyEventArgs e)
+        {
+            string fileName = counter + ".jpg";
+
+            try
+            {
+                using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    using (IsolatedStorageFileStream stream = isf.OpenFile(fileName, FileMode.Create, FileAccess.Write))
+                    {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead = -1;
+
+                        while ((bytesRead = e.ImageStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            stream.Write(buffer, 0, bytesRead);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                PhoneApplicationService.Current.State["fileName"] = fileName;
+                e.ImageStream.Close();
+
+                navigateToConfirmationPage();
+            }
+        }
+
+        private void navigateToConfirmationPage()
+        {
+            Dispatcher.BeginInvoke(() =>
+    NavigationService.Navigate(new Uri("/PhotoConfirmationView.xaml", UriKind.Relative)));
+        }
+
+        private void onButtonHalfPressHandler(object sender, EventArgs e)
+        {
+            if (camera != null && camera.IsFocusSupported == true)
+            {
+                try
+                {
+                    camera.Focus();
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void onButtonFullPressHandler(object sender, EventArgs e)
+        {
+            if (camera != null)
+            {
+                try
+                {
+                    camera.CaptureImage();
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void onButtonReleaseHandler(object sender, EventArgs e)
+        {
+            try
+            {
+                if (camera != null && camera.IsFocusSupported == true)
+                {
+                    camera.CancelFocus();
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void photoViewfinderCanvas_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            takePhotoButton_Click(this, null);
         }
     }
 }
---
+++ CSReportApp/CSReportApp/PhotoConfirmationView.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace CSReportApp
+{
+    public partial class PhotoConfirmationView : PhoneApplicationPage
+    {
+        private string fileName = "";
+
+        public PhotoConfirmationView()
+        {
+            InitializeComponent();
+
+            fileName = (string)PhoneApplicationService.Current.State["fileName"];
+            loadImage();
+        }
+
+        private void loadImage()
+        {
+            byte[] data;
+
+            try
+            {
+                using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    using (IsolatedStorageFileStream stream = isf.OpenFile(fileName, FileMode.Open, FileAccess.Read))
+                    {
+                        data = new byte[stream.Length];
+                        stream.Read(data, 0, data.Length);
+                        stream.Close();
+                    }
+                }
+
+                MemoryStream memoryStream = new MemoryStream(data);
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.SetSource(memoryStream);
+                imageBrush.ImageSource = bitmap;
+                imageBrush.RelativeTransform = new CompositeTransform() { CenterX = 0.5, CenterY = 0.5, Rotation = 90 };
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void RetakeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => NavigationService.GoBack());
+        }
+
+        private void AcceptButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
