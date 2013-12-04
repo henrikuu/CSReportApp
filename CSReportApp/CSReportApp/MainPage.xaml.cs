b19e78c213459b3b3fad25cd3fa3047ad9ae3e1e
Mikko Pisto <mikko.pisto@gmail.com>
2013-12-04 14:34:27 +0200

    GPS
    gps tsekkaus hoidetaan alustuksessa. Lisätty oikeudet tarkistaa gps

--- CSReportApp/CSReportApp/MainPage.xaml.cs
+++ CSReportApp/CSReportApp/MainPage.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using CSReportApp.Resources;
+using System.Device.Location;
 
 namespace CSReportApp
 {
@@ -17,7 +18,7 @@ namespace CSReportApp
         {
 
             InitializeComponent();
-            //tee tsekkaus gps
+            checkGPS();
         }
 
         private void menuButtonTakePicture_Click(object sender, RoutedEventArgs e)
@@ -33,6 +34,18 @@ namespace CSReportApp
         private void menuButtonTakeVideo_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Uri("/VideoCapturePage.xaml", UriKind.Relative));
-        } 
+        }
+
+        private void checkGPS()
+        {
+            GeoCoordinateWatcher g = new GeoCoordinateWatcher();
+            g.Start();
+
+            if (g.Permission.Equals(GeoPositionPermission.Denied) || g.Permission.Equals(GeoPositionPermission.Unknown))
+            {
+                MessageBox.Show("Location services are disabled. To enable them, Goto Settings - Location - Enable Location Services.", "Location services", MessageBoxButton.OK);
+                Application.Current.Terminate();
+            }
+        }
     }
 }
